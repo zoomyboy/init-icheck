47e8e0f50628f0883adc99de2177d32e741e2249
Philipp Lang <pille@stammsilva.de>
2016-11-10 17:24:14 +0100

    Initial Commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Zoomyboy\InitIcheck;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider {
+	public function boot() {}
+
+	public function register() {
+
+	}
+}
