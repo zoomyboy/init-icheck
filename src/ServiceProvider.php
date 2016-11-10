c9311621639b9421ad1a5589948cfc08c0f378ca
Philipp Lang <pille@stammsilva.de>
2016-11-10 18:09:15 +0100

    Add icheck

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,6 +8,7 @@ class ServiceProvider extends BaseServiceProvider {
 	public function boot() {}
 
 	public function register() {
-
+		$this->publishes([base_path('vendor/zoomyboy/init-icheck').'/assets/icheck/' => resource_path('assets/vendor/icheck')]);
+		$this->publishes([base_path('vendor/zoomyboy/init-icheck').'/assets/js/icheck.init.js' => resource_path('assets/vendor/icheck/icheck.init.js')]);
 	}
 }
